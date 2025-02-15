4590c556b5865a990da0f67efd71b0647458f8fb
Folastar <ogundelefolariin@gmail.com>
2025-02-15 20:16:19 +0100

    ticket modification

--- src/pages/TicketBook.jsx
+++ src/pages/TicketBook.jsx
@@ -11,7 +11,7 @@ const TicketBook = ({
 }) => {
   console.log(uploadedImageUrl);
   return (
-    <section className="w-full  ">
+    <section className="w-full relative  ">
       <div className="text-white pt-3 text-center mb-8  ">
         <h1 className="text-2xl font-jeju">Your Ticket is Booked</h1>
         <p className="font-jeju text-sm py-3 capitalize">
@@ -19,18 +19,18 @@ const TicketBook = ({
           check your email for a copy or you can download
         </p>
       </div>
-      <div className="parentCard  my-5 relative  w-[90%] mx-auto  p-4  mt-2  ">
-        <div className=" sm:w-[70%] w-full h-screen p-4 mx-auto rounded-2xl   ">
-          {/* <div>
-            <img src="/Heading.png"  className="my-5"alt="logo" />
+      <div className="parentCard  my-5 relative sm:w-[90%]  w-[100%] mx-auto  sm:p-4  mt-2  ">
+        <div className="border-2  border-mild-green sm:w-[90%]  w-[80%]  sm:h-[490px] h-[580px] p-4 mx-auto rounded-2xl   ">
+          <div>
+            <img src="/Heading.png"  className="my-5 px-4"alt="logo" />
             <p className='text-white text-center capitalize text-sm'> 📍 04 runners road, ikoyi, lagos <br /> march 15 2025 | 7:00pm</p>
-            </div> */}
+            </div>
           <div
             style={{ backgroundImage: `url(${uploadedImageUrl})` }}
-            className="sm:w-48 w-30 absolute   bg-image sm:top-40 top-36 left-0 right-0 sm:h-40 h-44 bg-mid-green border-mild-green border-3 rounded-lg  mx-auto my-4 "
+            className="sm:w-40 w-30 absolute    sm:top-35 left-0 right-0 sm:h-30 h-30 bg-mid-green border-mild-green border-3 rounded-lg  mx-auto my-4 "
           ></div>
 
-          <div className="  absolute sm:bottom-[145px] bottom-[175px] p-4 left-0 right-0 w-[65%] h-[30%] grid-cols-2 place-content-center bg-mid-green grid mt-10  mx-auto border-[#0e4a54] border-2 rounded-2xl">
+          <div className="  absolute sm:bottom-[145px] bottom-[195px] p-4 left-0 right-0 w-[60%] h-[30%] grid-cols-2 place-content-center bg-mid-green grid mt-10  mx-auto border-[#0e4a54] border-2 rounded-2xl">
             <div className="text-white text-sm  border-[#0e4a54] border-b-2 border-r-2 ">
               <span className="text-white/50  capitalize text-[10px]">
                 Enter your name
@@ -74,14 +74,14 @@ const TicketBook = ({
           </div>
         </div>
 
-        <div className="absolute bottom-0 left-0 right-0  inset-shadow-light-green w-[80%] mx-auto p-4   overflow-hidden ">
+      </div>
+        <div className="absolute bottom-25 left-0 right-0  inset-shadow-light-green w-[80%] mx-auto p-4   overflow-hidden ">
           <img
             src="/Bar Code.png"
-            className="mx-auto max-w-45"
+            className="mx-auto max-w-50"
             alt="bar code"
           />
         </div>
-      </div>
 
       <div className="btn sm:flex grid grid-cols-1 gap-y-3 justify-between items-center relative gap-x-3 py-3">
         <button
